bcee52fca936a1950ad63bd06bbfa15e3aeb5bb0
mike-a-yen <mike.a.yen@gmail.com>
2015-11-19 13:53:07 -0800

    testing ignore

--- Coursera/DataAnalysisTools/week1.py
+++ Coursera/DataAnalysisTools/week1.py
@@ -22,13 +22,15 @@ independent = 'REGION'
 df[dependent] = df[dependent].replace(999,np.nan)
 df = df.dropna(subset=[dependent])
 
-t1 = df[df[independent]==1][dependent]
-t2 = df[df[independent]==2][dependent]
-t3 = df[df[independent]==3][dependent]
-t4 = df[df[independent]==4][dependent]
+t1 = df[df[independent]==1]
+t2 = df[df[independent]==2]
+t3 = df[df[independent]==3]
+t4 = df[df[independent]==4]
 perms = permutations([t1,t2,t3,t4],r=2)
+print 'Regions    p-value'
 for perm in perms:
-    
+    f,p = stats.f_oneway(perm[0][dependent],perm[1][dependent])
+    print perm[0][independent], perm[1][independent],'    ',p
 
 results = pd.DataFrame(df[independent].unique(), columns=[independent])
 results['mean'] = results[independent].apply(lambda r: df[df[independent]==r][dependent].mean(skipna=True))
